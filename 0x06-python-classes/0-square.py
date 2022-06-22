684a548e73498fa34496c4cd5fcd1d51a56efdb6
Charity012 <sammycharity@gmail.com>
2022-06-21 18:53:05 -0700

    Task0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-"""Empty class Square that defines a square"""
-
-
 class Square:
-    """This class was purposefully left blank."""
+    """Represents an empty square."""
     pass
